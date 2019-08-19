fef59f8ca4fbd6fa5ecf07594f6f79077d78090e
ricardocappi <53310222+ricardocappi@users.noreply.github.com>
2019-08-19 18:25:18 -0300

    Ajuste do TabularInline para EstoqueItens no Estoque

--- projeto/estoque/admin.py
+++ projeto/estoque/admin.py
@@ -4,8 +4,14 @@ from .models import Estoque, EstoqueItens
 # Register your models here.
 
 
+class EstoqueItensInline(admin.TabularInline):
+    model = EstoqueItens
+    extra = 0
+
+
 @admin.register(Estoque)
 class EstoqueAdmin(admin.ModelAdmin):
+    inlines = (EstoqueItensInline, )
     list_display = (
         '__str__',
         'funcionario',
@@ -15,6 +21,8 @@ class EstoqueAdmin(admin.ModelAdmin):
     search_fields = ('nota_fiscal', )
     list_filter = ('funcionario',)
     date_hierarchy = 'created'
+
+
 
 @admin.register(EstoqueItens)
 class EstoqueItensAdmin(admin.ModelAdmin):
